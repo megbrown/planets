31431131678b424b1ac7ed1846080bda0cd8b22f
Megan B <brown.m.j@outlook.com>
2017-06-26 16:43:10 -0500

    completed exercise

--- array-methods.js
+++ array-methods.js
@@ -12,17 +12,26 @@ planets.forEach( function(currentPlanet) {
 
 
 // Use the map method to create a new array where the first letter of each planet is capitalized
-planets.map( function(currentPlanet, index) {
-	planets = currentPlanet.charAt(0).toUpperCase() + currentPlanet.substr(1);
-	capitalDiv.innerHTML += `<h3>${planets}</h3>`;
+let capitalPlanets = planets.map( function(currentPlanet) {
+	return currentPlanet.charAt(0).toUpperCase() + currentPlanet.substr(1);
+})
+
+console.log("capital planets", capitalPlanets);
+
+capitalPlanets.forEach( function(planet) {
+	capitalDiv.innerHTML += `<h3>${planet}</h3>`;
 })
 
 // Use the filter method to create a new array that contains planets with the letter 'e'
-// function checkE(planets) {
-// 	return planets.contains("e");
-// }
-// console.log(planets)
-// console.log(planets.filter(checkE))
+let ePlanets = planets.filter( function(currentPlanet) {
+	return currentPlanet.includes("e");
+})
+
+console.log("e planets", ePlanets)
+
+ePlanets.forEach( function(planet) {
+	filterDiv.innerHTML += `<h3>${planet}</h3>`;
+})
 
 // Use the reduce method to create a sentence from the words in the following array
 var words = ["The", "early", "bird", "might", "get", "the", "worm", "but", "the", "second", "mouse", "gets", "the", "cheese"];
